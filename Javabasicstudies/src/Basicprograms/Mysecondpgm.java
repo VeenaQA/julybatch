f36493e2e13c9aefcb873b6bedd7f476d711ca58
veena <veena@VeenaPC>
2024-07-01 11:56:30 +0530

    third commit

--- Javabasicstudies/src/Basicprograms/Mysecondpgm.java
+++ Javabasicstudies/src/Basicprograms/Mysecondpgm.java
@@ -5,6 +5,7 @@ public class Mysecondpgm {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my second program");
+		System.out.println("this is the third attempt");
 
 	}
 
