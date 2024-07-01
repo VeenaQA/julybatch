1c8a935119be54204b6332b77bb5b9221a126ebd
veena <veena@VeenaPC>
2024-07-01 11:39:34 +0530

    commiting the pgm

---
+++ Javabasicstudies/src/Basicprograms/MyfirstProgram.java
@@ -0,0 +1,10 @@
+package Basicprograms;
+
+public class MyfirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("this is my first program");
+	}
+
+}
