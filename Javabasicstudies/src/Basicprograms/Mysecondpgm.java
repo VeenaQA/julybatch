85756b9234e24e9e1a6e90138467396263b97300
veena <veena@VeenaPC>
2024-07-01 11:50:30 +0530

    second push

---
+++ Javabasicstudies/src/Basicprograms/Mysecondpgm.java
@@ -0,0 +1,11 @@
+package Basicprograms;
+
+public class Mysecondpgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my second program");
+
+	}
+
+}
